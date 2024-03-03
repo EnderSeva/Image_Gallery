509c572ca33f3cfb5b9e1150bc673878fcb7da67
EnderSeva <vsevolod.tsybizov@gmail.com>
2024-03-03 19:00:44 +0300

    Commit number 1: start

---
+++ script1.js
@@ -0,0 +1,20 @@
+let scrollContainer = document.querySelector(".gallery"),
+    backBtn = document.getElementById("backBtn"),
+    nextBtn = document.getElementById("nextBtn");
+
+scrollContainer.addEventListener("wheel", (evt) => {
+    evt.preventDefault();
+    scrollContainer.scrollLeft += evt.deltaY;
+    scrollContainer.style.scrollBehavior = "auto";
+
+});
+
+backBtn.addEventListener("click", () => {
+    scrollContainer.scrollLeft -= 900;
+    scrollContainer.style.scrollBehavior = "smooth";
+});
+
+nextBtn.addEventListener("click", () => {
+    scrollContainer.scrollLeft += 900;
+    scrollContainer.style.scrollBehavior = "smooth";
+})
